c6fd95334bab08e375e4233a597a0ea0b6617c94
Mae Rine <mae.s.rine@gmail.com>
2019-07-13 17:45:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list');
+  
+  for(let i = 0; i < rankedLists.length; i++){
+    var children = rankedLists[i].children;
+  
+    for(let j = 0; j < children.length; j++){
+      var rank = parseInt(children[j].innerHTML);
+      rank += n;
+      children[j].innerHTML = rank.toString();
+    }
+  }
+}
+
+function deepestChild() {
+  const grand = document.querySelector('div#grand-node');
+  var current = grand;
+  var next = 
+}
